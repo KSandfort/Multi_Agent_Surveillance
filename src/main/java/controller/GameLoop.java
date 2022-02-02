b886f0e384b79c90dcbb467fd62cf31db047d638
k-san-official <konstantin.sandfort@gmail.com>
2022-02-02 20:26:59 +0100

    GameLoop changes

--- src/main/java/controller/GameLoop.java
+++ src/main/java/controller/GameLoop.java
@@ -5,8 +5,13 @@ package controller;
  */
 public class GameLoop {
 
+    // Variables
+    private final static boolean DEBUG = true;
     private boolean running;
     private final double updateRate = 1.0d/60.0d;
+    private long nextStatTime;
+    private int fps;
+    private int ups;
 
     public void run() {
         running = true;
@@ -24,12 +29,25 @@ public class GameLoop {
                 accumulator -= updateRate;
             }
             render();
+            if (DEBUG)
+                printStats();
+        }
+    }
+
+    private void printStats() {
+        if(System.currentTimeMillis() > nextStatTime) {
+            System.out.println(String.format(""));
+            fps = 0;
+            ups = 0;
+            nextStatTime = System.currentTimeMillis() + 1000;
         }
     }
 
     private void render() {
+        fps++;
     }
 
     private void update() {
+        ups++;
     }
 }
