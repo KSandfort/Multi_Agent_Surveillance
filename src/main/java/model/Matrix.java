55da6f742f7168eb234184926af52b0c956c2830
WillemP1 <willem.ploegstra@gmail.com>
2022-02-19 14:44:07 +0100

    Matrix implementation, want to test NN controller

---
+++ src/main/java/model/Matrix.java
@@ -0,0 +1,117 @@
+package model;
+
+public class Matrix {
+    public double[][] values;
+    public int rows;
+    public int columns;
+    public Matrix(int x,int y)
+    {
+        rows = x;
+        columns = y;
+        values = new double[x][y];
+    }
+
+    public Matrix(double[][] values)
+    {
+        this.values = values;
+        rows = values.length;
+        columns = values[0].length;
+    }
+
+    public void add(double value)
+    {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                values[i][j] += value;
+            }
+        }
+    }
+
+    public void add(Matrix m)
+    {
+        if(rows != m.rows || columns != m.columns)
+        {
+            throw new IllegalArgumentException("Shape of matrices does not match ")
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                values[i][j] += m.values[i][j];
+            }
+        }
+    }
+
+    public void subtract(double value)
+    {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                values[i][j] -= value;
+            }
+        }
+    }
+
+    public void subtract(Matrix m)
+    {
+        if(rows != m.rows || columns != m.columns)
+        {
+            throw new IllegalArgumentException("Shape of matrices does not match ")
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                values[i][j] -= m.values[i][j];
+            }
+        }
+    }
+
+    //matrix multiplication
+    public Matrix mMultiply(Matrix m)
+    {
+        if(m.rows != columns)
+        {
+            throw new IllegalArgumentException("Shapes of matrices are incorrect");
+        }
+
+        Matrix result = new Matrix(rows,m.columns);
+
+        for(int i=0;i<result.rows;i++)
+        {
+            for(int j=0;j<result.columns;j++)
+            {
+                double sum=0;
+                for(int k=0;k<columns;k++)
+                {
+                    sum+=values[i][k]*m.values[k][j];
+                }
+                result.values[i][j]=sum;
+            }
+        }
+        return result;
+    }
+
+
+    //element wise multiplication
+    public void eMultiply(Matrix m)
+    {
+        if(rows != m.rows || columns != m.columns)
+        {
+            throw new IllegalArgumentException("Shape of matrices does not match ")
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                values[i][j] *= m.values[i][j];
+            }
+        }
+    }
+
+    //multiply by scalar
+    public void sMultiply(double a)
+    {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                values[i][j] *= a;
+            }
+        }
+    }
+}
