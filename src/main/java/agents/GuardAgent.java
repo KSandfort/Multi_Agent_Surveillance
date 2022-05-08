1ee4d416fbbad0982267e3ab2ef8e5c4977210f7
maviles1 <mgmbamartin@gmail.com>
2022-05-09 00:34:26 +0200

    s

---
+++ src/main/java/agents/GuardAgent.java
@@ -0,0 +1,17 @@
+package agents;
+
+import Enities.Entity;
+import Enities.Intruder;
+import model.MapItem;
+
+import java.util.ArrayList;
+
+public class GuardAgent extends AbstractAgent{
+    @Override
+    public void addControls() {}
+
+    @Override
+    public void changeMovement(ArrayList<MapItem> items) {
+
+    }
+}
